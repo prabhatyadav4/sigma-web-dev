de8e9a2e5da3aea358682566fbf098da2b3cad90
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-05-17 06:01:29 +0530

    VIDEO 57 Completed

--- VIDEO 57/script.js
+++ VIDEO 57/script.js
@@ -1,5 +1,7 @@
-console.log("This tutorial is a part of SIGMA Web Development Course")
+console.log("This tutorial is a part of SIGMA Web Development Course") // Print course info
+
 let a = 1;
+// For loop: prints numbers from a to a+9
 for (let i = 0; i < 10; i++){
     console.log(a+i);
 }
@@ -10,22 +12,26 @@ let obj = {
     "salary" : "$3400"
 }
 
+// For-in loop: iterate over object properties
 for (const key in obj){
     const element = obj[key];
     console.log(key, element)
 }
 
+// For-of loop: iterate over each character in string
 for (const c of "Harry"){
     console.log(c)
 }
 
 let i = 2;
+// While loop: prints numbers from 2 to 5
 while(i<6){
     console.log(i)
     i++;    
 }
 
 let j = 0;
+// Do-while loop: prints numbers from 0 to 5
 do{
     console.log(j)
     j++;
---
+++ VIDEO 58/script.js
@@ -0,0 +1,7 @@
+function nice(name){
+    console.log("Hey " + name + ", you are good.")
+    console.log("Hey " + name + ", you are better.")
+    console.log("Hey " + name + ", you are best.")
+    console.log("Hey " + name + ", you are nice.")
+}
+nice("Prabhat")
