c575d8256227dc32dd788a96bad59a24bed380eb
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-05-22 19:04:59 +0530

    Video 60 Completed

--- VIDEO 60/script.js
+++ VIDEO 60/script.js
@@ -1,29 +1,27 @@
-console.log("Welcome to the tutorial 60 of SIGMA Web Development Course")
+console.log("Welcome to the tutorial 60 of SIGMA Web Development Course") // Print welcome message
 
-let friend1 = "Harry"
-let friend2 = "Rohan"
+let friend1 = "Harry" // Friend 1 name
+let friend2 = "Rohan" // Friend 2 name
 
-console.log("My name is " + friend1 + " and my friend name is " + friend2)
+console.log("My name is " + friend1 + " and my friend name is " + friend2) // String concatenation
 
 // Template Literal
-
-console.log(`My name is ${friend1} and my friend name is ${friend2}`)
+console.log(`My name is ${friend1} and my friend name is ${friend2}`) // Template literal
 
 // Escape Sequence
+console.log("His pet name is \"ROHU\"") // Escape double quotes
 
-console.log("His pet name is \"ROHU\"")
+let myName = "Prabhat" // My name
 
-let myName = "Prabhat"
+console.log(myName.toUpperCase()) // Uppercase
+console.log(myName.toLocaleLowerCase) // Lowercase (missing parentheses)
+console.log(myName.length) // Length of string
+console.log(myName.slice(2,6)) // Slice from 2 to 6
+console.log(myName.slice(2)) // Slice from 2 to end
+console.log(myName.replace("P","S")) // Replace P with S
+console.log(myName.concat(friend1, friend2, "Aishwarya")) // Concatenate strings
 
-console.log(myName.toUpperCase())
-console.log(myName.toLocaleLowerCase)
-console.log(myName.length)
-console.log(myName.slice(2,6))
-console.log(myName.slice(2))
-console.log(myName.replace("P","S"))
-console.log(myName.concat(friend1, friend2, "Aishwarya"))       // We can also use '+' operator for concat
-
-let yourName = "  Sahil  "
-console.log(yourName.trim())
-console.log(yourName.endsWith("t"))
-console.log(yourName.startsWith("P"))
+let yourName = "  Sahil  " // Another name
+console.log(yourName.trim()) // Trim spaces
+console.log(yourName.endsWith("t")) // Ends with 't'
+console.log(yourName.startsWith("P")) // Starts with 'P'
