ddf2a0b46d8430c7c0cbb7dd2ea47c206ef4e148
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-06-13 00:53:42 +0530

    Initial commit: uploaded SIGMA Web Dev course files

---
+++ VIDEO 67/script.js
@@ -0,0 +1,5 @@
+console.log("hello") // Print "hello" to the console
+console.log(document.body) // Log the <body> element of the document
+console.log(document.body.childNodes) // Log all child nodes of the <body>
+console.log(document.body.childNodes[1]) // Log the second child node of <body>
+console.log(document.body.childNodes[1].childNodes[1]) // Log the second child node of the above node
