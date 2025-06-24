6b79e41409aefd7625723ce3f14a6dfbb1e37bba
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-06-25 03:47:45 +0530

    Initial commit: uploaded SIGMA Web Dev course files

---
+++ VIDEO 86/main.js
@@ -0,0 +1,11 @@
+import {a,b,c,d,e} from "./mymodule.js"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+
+
+import harry from "./mymodule.js"
+console.log(harry)
---
+++ VIDEO 86/mymodule.js
@@ -0,0 +1,12 @@
+export const a  = 1 // Named export
+export const b  = 2
+export const c  = 3
+export const d  = 4
+export const e  = 5
+
+const obj = {
+    x : 4,
+    y : 5
+}
+
+export default obj; // Default export
