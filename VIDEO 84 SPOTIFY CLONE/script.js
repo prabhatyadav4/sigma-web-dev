cd1da0a85cde162f5c4fb2a22a352ce0efb1c996
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-07-31 16:37:08 +0530

    Initial commit: uploaded SIGMA Web Dev course files

--- VIDEO 84 SPOTIFY CLONE/script.js
+++ VIDEO 84 SPOTIFY CLONE/script.js
@@ -10,7 +10,7 @@ async function getSongs(){
     for(let index = 0; index < as.length; index++){
         const element = as[index];
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
@@ -20,6 +20,22 @@ async function main(){
 
     let songs = await getSongs()
     console.log(songs)
+
+    let songUL = document.querySelector('.songList').getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `
+         <li>
+                <img class="invert" src="/img/music.svg" alt="">
+                <div class="info">
+                  <div>${song.replaceAll("%20"," ")}</div>
+                  <div>Harry</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="/img/play.svg" alt="">
+                </div>
+              </li>`;
+    }
 
     var audio = new Audio(songs[0]);
     // audio.play();
