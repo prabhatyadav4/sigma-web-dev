e676b7f5fea46bae370de03c9b224a99cd5caf77
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-08-03 22:07:28 +0530

    Initial commit: uploaded SIGMA Web Dev course files

--- VIDEO 84 SPOTIFY CLONE/script.js
+++ VIDEO 84 SPOTIFY CLONE/script.js
@@ -1,7 +1,8 @@
 console.log('Lets write JavaScript');
-
+let currentSong = new Audio()
 async function getSongs(){
-    let a = await fetch("http://127.0.0.1:5500/songs/")    let response = await a.text().catch(error => {console.error("Error fetching songs:", error); return "<div>Failed to load songs</div>";});
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
@@ -14,36 +15,60 @@ async function getSongs(){
     }
     return songs
 }
-
-
 const playMusic = (track) => {
-    let audio = new Audio(`/songs/${encodeURIComponent(track)}`);
-    audio.play().catch(error => console.error("Autoplay prevented:", error));
+    currentSong.src = "/songs/"+track
+    currentSong.play()
+    play.src = "/img/pause.svg"
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
- 
+
 async function main(){
+    
+    
 
     let songs = await getSongs()
     console.log(songs)
 
     let songUL = document.querySelector('.songList').getElementsByTagName("ul")[0]
     for (const song of songs) {
-        let listItem = document.createElement('li');
-        listItem.innerHTML = `
+        songUL.innerHTML = songUL.innerHTML + `
          <li>
                 <img class="invert" src="/img/music.svg" alt="">
                 <div class="info">
                   <div>${song.replaceAll("%20"," ")}</div>
                   <div>Harry</div>
                 </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class="invert" src="/img/play.svg" alt="">
+                </div>
               </li>`;
-              listItem.addEventListener("click", () => {
-            playMusic(song);
+    }
+
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
         })
-        songUL.appendChild(listItem);
-   
     })
 
+    // Attach an event listener to previous, play and next
+    play.addEventListener("click", ()=> {
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "/img/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "/img/play.svg"
+        }
+    })
 
+    //Listen for time update event 
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration)
+    })
 }
 main()
