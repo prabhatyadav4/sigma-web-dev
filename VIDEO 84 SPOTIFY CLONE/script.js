c3d45fb9a523277ca85806356eae381c0fd6b3c6
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-08-04 03:10:55 +0530

    Initial commit: uploaded SIGMA Web Dev course files

--- VIDEO 84 SPOTIFY CLONE/script.js
+++ VIDEO 84 SPOTIFY CLONE/script.js
@@ -1,5 +1,20 @@
 console.log('Lets write JavaScript');
 let currentSong = new Audio()
+
+function secondsToMinutesSeconds(seconds){
+    if(isNaN(seconds) || seconds < 0){
+        return "Invalid input"
+    }
+
+    const minutes = Math.floor(seconds/60);
+    const remainingSeconds = Math.floor(seconds%60)
+
+    const formattedMinutes = String(minutes).padStart(2,'0')
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0')
+
+    return `${formattedMinutes}:${formattedSeconds}`
+}
+
 async function getSongs(){
     let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
@@ -15,11 +30,13 @@ async function getSongs(){
     }
     return songs
 }
-const playMusic = (track) => {
+const playMusic = (track, pause = false) => {
     currentSong.src = "/songs/"+track
-    currentSong.play()
-    play.src = "/img/pause.svg"
-    document.querySelector(".songinfo").innerHTML = track
+    if(!pause){
+        currentSong.play()
+        play.src = "/img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
 }
 
@@ -28,7 +45,7 @@ async function main(){
     
 
     let songs = await getSongs()
-    console.log(songs)
+    playMusic(songs[0], true)
 
     let songUL = document.querySelector('.songList').getElementsByTagName("ul")[0]
     for (const song of songs) {
@@ -69,6 +86,27 @@ async function main(){
     //Listen for time update event 
     currentSong.addEventListener("timeupdate", () => {
         console.log(currentSong.currentTime, currentSong.duration)
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100% + "%"
     })
+
+    // Add an event listener for seekbar
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+    // Add an event listener for hamburger
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0"
+    })
+
+    // Add an event listener for close button
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-110%"
+    })
+
 }
 main()
